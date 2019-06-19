15dc08baa55a4d713de73ab8b939d185c9298c00
Harsh Mistry <hnmistry@uwaterloo.ca>
2019-06-19 02:30:55 -0400

    Initial Commit

---
+++ app/src/main/java/ca/uwaterloo/cs446/teamdroids/technosync/MainMenu.java
@@ -0,0 +1,35 @@
+package ca.uwaterloo.cs446.teamdroids.technosync;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    public Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        //Get Buttons
+        startButton = (Button) findViewById(R.id.startButton);
+
+        //Setup Drum Pad Link
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open Drum Pad
+                Intent drumPadIntent = new Intent(getBaseContext(), BeatPad.class);
+
+                //Clear activity stack and start new activity
+                drumPadIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(drumPadIntent);
+            }
+        });
+
+    }
+}
