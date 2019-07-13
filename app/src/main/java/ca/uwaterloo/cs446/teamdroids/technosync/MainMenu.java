b2ee187c1179c849a87eb4e0460468976a30e889
Rohan Dave <rohandave31@gmail.com>
2019-07-13 01:47:47 -0400

    Functionality to create a group and start recording session
    API calls are yet to be implemented

--- app/src/main/java/ca/uwaterloo/cs446/teamdroids/technosync/MainMenu.java
+++ app/src/main/java/ca/uwaterloo/cs446/teamdroids/technosync/MainMenu.java
@@ -1,19 +1,29 @@
 package ca.uwaterloo.cs446.teamdroids.technosync;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainMenu extends AppCompatActivity {
 
-    public Button startGroupSessionButton;
+    public Button createGroupSessionButton;
+    public Button joinGroupSessionButton;
     public Button practiceButton;
     public Button recordCustomBeatButton;
     public Button changePresetsButton;
     public Button viewRecordingsButton;
+
+    public Button startGroupSession;
 
     //Animate background
     private void animateBackground(){
@@ -36,8 +46,10 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         //Get Buttons
-        startGroupSessionButton = (Button) findViewById(R.id.createGroupButton);
-        setupGroupSessionButton();
+        createGroupSessionButton = (Button) findViewById(R.id.createGroupButton);
+        setupCreateGroupSessionButton();
+        joinGroupSessionButton = (Button) findViewById(R.id.joinGroupButton);
+        setupJoinGroupSessionButton();
         practiceButton = (Button) findViewById(R.id.practiceButton);
         setupPracticeButton();
         recordCustomBeatButton = (Button) findViewById(R.id.recordAudioButton2);
@@ -51,8 +63,56 @@ public class MainMenu extends AppCompatActivity {
         animateBackground();
     }
 
-    private void setupGroupSessionButton() {
+    private void setupCreateGroupSessionButton() {
+        createGroupSessionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.create_group_popup_view, null);
+
+                // create the popup window
+                int width = 800;
+                int height = 700;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+                startGroupSession = (Button) popupView.findViewById(R.id.startGroupSession);
+
+                // Closes the popup window when touch outside.
+                popupWindow.setOutsideTouchable(true);
+                popupWindow.setFocusable(true);
+                // Removes default background.
+                popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                // Setup button to start the recording session
+                setupStartGroupSessionButton(popupWindow);
+            }
+        });
+    }
+
+    private void setupJoinGroupSessionButton() {
         // TODO: Implement
+    }
+
+    private void setupStartGroupSessionButton(PopupWindow popupWindow) {
+        // TODO: Implement
+        startGroupSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open Music  Creation Window
+                Intent drumPadIntent = new Intent(getBaseContext(), CreationView.class);
+
+                //Clear activity stack and start new activity
+                startActivity(drumPadIntent);
+
+                // Close the current popup
+                popupWindow.dismiss();
+            }
+        });
     }
 
     private void setupPracticeButton() {
